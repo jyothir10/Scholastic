6867a5b170a8db65a8c278be32c354fdb7b0df7e
jyothir10 <j.adithyan16@gmail.com>
2020-05-05 21:50:16 +0530

    yoo

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -76,7 +76,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                 Navigator.pushNamed(context, LoginScreen.id);
               },
               color: Colors.lightBlueAccent,
-              text: 'Loggg In',
+              text: 'Log In',
             ),
             RoundedButton(
               color: Colors.blueAccent,
