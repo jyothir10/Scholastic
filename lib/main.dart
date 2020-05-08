1a0fc553d05d1778d1efd4e63408f55fb3be64dd
jyothir10 <j.adithyan16@gmail.com>
2020-05-08 22:46:00 +0530

    yoo

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class Scholastic extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: ChatList.id,
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
